a230fc4d462a897cb4b315c8de1022deae0fb3b9
Sakesh Pusuluri <63548250+Sakesh-Pusuluri@users.noreply.github.com>
2021-12-31 15:00:23 -0500

    Update maximalSquare.py

--- maximalSquare.py
+++ maximalSquare.py
@@ -8,7 +8,7 @@ class Solution:
         for i in range(1,len(matrix)+1):
             for j in range(1,len(matrix[0])+1):
                 if(matrix[i-1][j-1]=='0'):
-                    dp[i][j]=0 # recurrence relation
+                    dp[i][j]=0 # recurrence relations
                 else:
                     dp[i][j] = 1 + min(dp[i-1][j-1] ,dp[i][j-1], dp[i-1][j]) # recurrence relation
                 ans = max(ans,dp[i][j]**2)
