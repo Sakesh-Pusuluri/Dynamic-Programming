95ba443887d04cfb225404399098775c8315ca40
Sakesh Pusuluri <63548250+Sakesh-Pusuluri@users.noreply.github.com>
2022-01-04 23:44:51 -0500

    Create palindromePartitioning.py

---
+++ palindromePartitioning.py
@@ -0,0 +1,15 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        result=[]
+        def backtrack(string,curr_list=[]):
+            if not string:
+                result.append(curr_list)
+                return
+            for i in range(1,len(string)+1):
+                _str = string[:i]
+                if(_str==_str[::-1]):
+                    backtrack(string[i:],curr_list+[string[:i]])
+        backtrack(s)
+        return result
+        
+       
