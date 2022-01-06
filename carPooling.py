17a45580105806f84813e699d6d98f3c45f4ce27
Sakesh Pusuluri <63548250+Sakesh-Pusuluri@users.noreply.github.com>
2022-01-05 22:49:49 -0500

    Create carPooling.py

---
+++ carPooling.py
@@ -0,0 +1,17 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        trips = sorted(trips, key=lambda x: x[1])
+        heap = []
+        rem_cap = 0
+
+        for trip in trips:
+            while heap and heap[0][0] <= trip[1]:
+                 rem_cap -= heappop(heap)[-1]
+
+            rem_cap += trip[0]
+            if rem_cap > capacity:
+                return False
+            trip = (trip[2], trip[1], trip[0])
+            heappush(heap, trip)
+
+        return True
