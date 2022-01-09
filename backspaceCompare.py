ce2a822b2382ba8afc8d59417858c2453a971ee7
Sakesh Pusuluri <63548250+Sakesh-Pusuluri@users.noreply.github.com>
2022-01-09 12:29:07 -0500

    Create backspaceCompare.py

---
+++ backspaceCompare.py
@@ -0,0 +1,21 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        l1,l2=[],[]
+        def listString(l,string):
+            for i in string:
+                if(i=='#' and l):
+                    l.pop()
+                elif(i!='#'):
+                    l.append(i)
+            return l
+        l1=listString(l1,s)
+        l2=listString(l2,t)
+        print(l1, l2)
+        if(l1==l2):
+            return True
+        return False
+
+      
+      
+# Time complexity - O(N)
+# Space complexity - O(N)
