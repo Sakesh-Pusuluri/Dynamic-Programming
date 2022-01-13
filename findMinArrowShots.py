56ca5761987d7bbf69fc419ec0b7ed419d4b11a9
Sakesh Pusuluri <63548250+Sakesh-Pusuluri@users.noreply.github.com>
2022-01-12 23:19:39 -0500

    Create findMinArrowShots.py

---
+++ findMinArrowShots.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        if not points:
+            return 0
+        points.sort(key=lambda x:x[1])
+        arr = 1
+        firstEnd=points[0][1]
+        for start,end in points:
+            if(firstEnd<start):
+                arr+=1
+                firstEnd=end
+        return arr
+        
+        
+# Time complexity - O(NlogN)
+# Space complexity - O(N)
