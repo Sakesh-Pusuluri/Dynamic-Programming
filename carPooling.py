9c45b1739976350eb9605742fa49adae7f858652
Sakesh Pusuluri <63548250+Sakesh-Pusuluri@users.noreply.github.com>
2022-01-18 18:34:32 -0500

    Update carPooling.py

--- carPooling.py
+++ carPooling.py
@@ -1,5 +1,5 @@
 class Solution:
-    def carPooling(self, trips: List[List[int]], capacity: int):
+    def carPooling(self, trips, capacity):
         trips = sorted(trips, key=lambda x: x[1])
         heap = []
         rem_cap = 0
