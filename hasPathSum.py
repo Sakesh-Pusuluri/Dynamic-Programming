a17ffe6da696e9a6e21ed4ae168ad38eb45c0f83
Sakesh Pusuluri <63548250+Sakesh-Pusuluri@users.noreply.github.com>
2022-01-26 15:17:59 -0500

    Create hasPathSum.py

---
+++ hasPathSum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if root is None:
+            return
+        targetSum-=root.val
+        if root.left is None and root.right is None:
+            return targetSum==0
+        return self.hasPathSum(root.left,targetSum) or self.hasPathSum(root.right,targetSum)
+      
+      
+# Time complexity - O(N)
+# Space complexity - O(N)
