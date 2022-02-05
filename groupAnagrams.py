9286a3debaec6fb70c8ce93e5a8ae0c4dab0b16f
Sakesh Pusuluri <63548250+Sakesh-Pusuluri@users.noreply.github.com>
2022-02-04 21:37:19 -0500

    Update groupAnagrams.py

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
         result = collections.defaultdict(list)
         for word in strs:
             result[tuple(sorted(word))].append(word)
