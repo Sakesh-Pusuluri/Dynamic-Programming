d0145e64c5174006a3080935e36b52d1f84900ed
Sakesh Pusuluri <63548250+Sakesh-Pusuluri@users.noreply.github.com>
2022-02-08 22:51:37 -0500

    Create QuickFind.py

---
+++ QuickFind.py
@@ -0,0 +1,19 @@
+class QuickFind:
+    def __init__(self,size):
+        self.root=[i for i in range(size)]
+    def find(self, x):
+        return self.root[x]
+    def union(self,x,y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if(rootX!=rootY):
+            for i in range(len(self.root)):
+                if(self.root[i]==rootY):
+                    self.root[i]=rootX
+    def connected(self,x,y):
+        return self.find(x)==self.find(y)
+      
+      
+# find complexity - O(1)
+# union complexity - O(N)
+# connected complexity - O(1)
