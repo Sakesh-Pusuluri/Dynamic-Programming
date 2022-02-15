b11f65ee5da31c6b564b5e9738426d52b003a8ef
Sakesh Pusuluri <63548250+Sakesh-Pusuluri@users.noreply.github.com>
2022-02-15 14:57:30 -0500

    Create distinctNumbers.py

---
+++ distinctNumbers.py
@@ -0,0 +1,19 @@
+class Solution:
+    def distinctNumbers(self, nums: List[int], k: int) -> List[int]:
+        ans=[]
+        numDict={}
+        i,j=0,0
+        while(j<len(nums)):
+            numDict[nums[j]] = numDict.get(nums[j],0)+1
+            j+=1
+            if(j-i==k):
+                ans.append(len(numDict))
+                if(numDict[nums[i]]>1):
+                    numDict[nums[i]]-=1
+                else:
+                    del numDict[nums[i]]
+                i+=1
+        return ans 
+            
+            
+            
