d9c26e04c28ea1436a19ba2f91c6a1ad926ad05d
Sakesh Pusuluri <63548250+Sakesh-Pusuluri@users.noreply.github.com>
2022-03-08 23:50:50 -0500

    Update findMinArrowShots.py

--- findMinArrowShots.py
+++ findMinArrowShots.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findMinArrowShots(self, points: List[List[int]]) -> int:
+    def findMinArrowShots(self, points):
         if not points:
             return 0
         points.sort(key=lambda x:x[1])
