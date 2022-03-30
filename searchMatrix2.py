ef5dc6400e80a8bf0d7f4226aa371952216ee5b9
Sakesh Pusuluri <63548250+Sakesh-Pusuluri@users.noreply.github.com>
2022-03-29 23:22:01 -0400

    Create searchMatrix2.py

---
+++ searchMatrix2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m=len(matrix)
+        if(m==0):
+            return False
+        n = len(matrix[0])
+        l,h=0,m*n-1
+        while(l<=h):
+            mid=(l+h)//2
+            if(matrix[mid//n][mid%n]==target):
+                return True
+            elif(matrix[mid//n][mid%n]<target):
+                l = mid+1
+            else:
+                h =mid-1
+        return False
